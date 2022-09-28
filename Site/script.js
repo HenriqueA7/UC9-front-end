0ab3164aee91e3573b4d7887caab9fbc1f3f6006
HenriqueA7 <riqueaus@gmail.com>
2022-09-28 12:16:58 -0300

    adicionado JS, Bootstrap, menu hamburguer

---
+++ Site/script.js
@@ -0,0 +1,16 @@
+let menu = document.getElementById("menu")
+
+function mostrarMenu (){
+    if(menu.style.display == "none"){
+        menu.style.display = "flex"
+    }else{
+        menu.style.display = "none"
+    }
+}
+
+let email = document.getElementById("campo-email")
+
+function enviarEmail(){
+    let emailDigitado = email.value 
+    console.log(emailDigitado)
+}
